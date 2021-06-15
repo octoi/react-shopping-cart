d66a5d759467617e559ea38c12b89729cb695ed4
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-15 14:50:08 +0530

    basic cart item component

---
+++ src/cartItem/CartItem.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { CartItemType } from '../utils/types';
+import { Button } from '@material-ui/core';
+import { Wrapper } from './CartItem.styles';
+
+const CartItem: React.FC = () => <div>Cart Item</div>
+
+export default CartItem;
