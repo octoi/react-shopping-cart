729974f9a649d0b4c2add930ddc871d5b98e0761
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-15 13:11:02 +0530

    basic component style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { useState } from 'react';
 import { useQuery } from 'react-query';
-// components
+// Utils
+import { productsUrl } from './utils/constants';
+// Components
 import Drawer from '@material-ui/core/Drawer';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import Grid from '@material-ui/core/Grid';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import Badge from '@material-ui/core/Badge';
+// Styles
+import { Wrapper } from './App.styles';
+
+const getProducts = async () => {
+    await (await fetch(productsUrl)).json();
+}
 
 export default function App() {
     return (
