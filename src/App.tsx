4fd47c75799995460d100e378f831d3373ecb850
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-15 13:16:07 +0530

    used type

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useQuery } from 'react-query';
 // Utils
 import { productsUrl } from './utils/constants';
+import { CartItemType } from './utils/types';
 // Components
 import Drawer from '@material-ui/core/Drawer';
 import LinearProgress from '@material-ui/core/LinearProgress';
@@ -11,8 +12,8 @@ import Badge from '@material-ui/core/Badge';
 // Styles
 import { Wrapper } from './App.styles';
 
-const getProducts = async () => {
-    await (await fetch(productsUrl)).json();
+const getProducts = async (): Promise<CartItemType> => {
+    return await (await fetch(productsUrl)).json();
 }
 
 export default function App() {
