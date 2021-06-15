32d41497f152cc942feb3348cb5cbca2df562f91
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-15 15:15:16 +0530

    remove from cart

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,18 @@ export default function App() {
         });
     };
 
-    const handleRemoveFromCart = (id: number) => null;
+    const handleRemoveFromCart = (id: number) => {
+        setCartItems(prev => (
+            prev.reduce((ack, item) => {
+                if (item.id === id) {
+                    if (item.amount === 1) return ack;
+                    return [...ack, { ...item, amount: item.amount - 1 }];
+                } else {
+                    return [...ack, item];
+                }
+            }, [] as CartItemType[])
+        ))
+    };
 
     if (isLoading) return <LinearProgress />
     if (error) return <div>something went wrong</div>
